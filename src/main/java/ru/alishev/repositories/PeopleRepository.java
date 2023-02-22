bc8c55f173020cbc3df8083996d93e57bfb6659e
Tamer00775 <tamerlankartaev04@gmail.com>
2023-02-22 14:39:12 +0600

    Some errors

---
+++ src/main/java/ru/alishev/repositories/PeopleRepository.java
@@ -0,0 +1,9 @@
+package ru.alishev.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.alishev.models.Person;
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+
+}
